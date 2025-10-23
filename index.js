52b28f7d6ea95094fb76ed0441e4be65f49a0f9d
PcsaDev <omarsaidortiz09@gmail.com>
2025-10-22 23:03:52 -0600

    esqueleto

---
+++ index.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    const today = new Date();
+    const DiaUno = new Date('2024-04-16');
+    console.log(today)
+    const DifMes = today - DiaUno;
+    const DiasJuntos = Math.ceil(DifMes / (1000 * 60 * 60 * 24));
+
+
+    document.title = DiasJuntos - 2; //Te amooo
+}
